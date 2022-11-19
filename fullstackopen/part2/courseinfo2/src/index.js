c6c0ba3880db55ed88bee723373b87bb51631aef
dejaimenpax <jaimegonzalezcasero@gmail.com>
2022-11-19 12:21:16 +0100

    Add part2.1

---
+++ fullstackopen/part2/courseinfo2/src/index.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) =>
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) =>
+  <>
+    {parts.map( x => <Part key={x.id} part={x} />)}
+  </>
+
+const Course = ({ course }) =>
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </>
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  }
+
+  return <Course course={course} />
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
