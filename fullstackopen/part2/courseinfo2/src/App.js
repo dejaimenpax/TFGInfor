5b697344cf43eede12147195c6c95f6994a59b7f
dejaimenpax <jaimegonzalezcasero@gmail.com>
2022-11-20 01:08:23 +0100

    Add part 2.8

--- fullstackopen/part2/courseinfo2/src/App.js
+++ fullstackopen/part2/courseinfo2/src/App.js
@@ -5,10 +5,12 @@ const App = () => {
   const [persons, setPersons] = useState([
     { 
       name: 'Arto Hellas',
+      number: '39-445323523',
       id: 1
     }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -30,12 +32,14 @@ const App = () => {
     else{
       const personObject = {
         name: newName,
+        number: newNumber,
         id: persons.length + 1,
       }
   
       setPersons(persons.concat(personObject))
     }
 
+    setNewNumber('')
     setNewName('')
   }
 
@@ -44,24 +48,38 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   return (
-    <div>
+    <>
+
       <h2>Phonebook</h2>
+
       <form onSubmit={addPerson}>
-        <div>
-          name: 
+
+        <div>name: 
           <input 
             value={newName}
             onChange={handleNameChange}
           />
         </div>
-        <div>
-          <button type="submit">add</button>
+
+        <div>number:
+          <input 
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
         </div>
+
+        <div><button type="submit">add</button></div>
+
       </form>
-      <h2>Numbers</h2>
+
       {persons.map( x => <Person key={x.id} person={x} />)}
-    </div>
+
+    </>
   )
 }
 
